7a6b810b5b303f418cef04390d7f92348465e27c
Arunesh-etc15 <arunesh.etc15@gmail.com>
2025-06-09 09:06:54 +0530

    01 changes

--- abc.java
+++ abc.java
@@ -3,5 +3,11 @@ public class Test{
     public void TestCode{
         
     }
+<<<<<<< Updated upstream
     public int getCount;
+=======
+
+    public String getName;
+
+>>>>>>> Stashed changes
 }
